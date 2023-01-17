d9a10a2c6b585ca5ea06bab07ad8b431786387fd
Yuxuan Chen <chenyx0512@gmail.com>
2023-01-17 08:50:03 -0500

    Add submit8 for testing

---
+++ src/submit8/Constants.java
@@ -0,0 +1,27 @@
+package submit8;
+
+import battlecode.common.Direction;
+
+import java.util.Random;
+
+public class Constants {
+    /** Array containing all the possible movement directions. */
+    public static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+    /**
+     * A random number generator.
+     * We will use this RNG to make some random moves. The Random class is provided by the java.util.Random
+     * import at the top of this file. Here, we *seed* the RNG with a constant number (6147); this makes sure
+     * we get the same sequence of numbers every time this code is run. This is very useful for debugging!
+     */
+    static final Random rng = new Random(6147);
+}

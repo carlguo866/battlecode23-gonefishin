7c57b3fb3eaa14394f42b1d1d3793942c7eb7da8
Carl Guo <carlguo866@gmail.com>
2023-01-20 02:42:18 -0500

    merge micro into submit11

---
+++ src/micro/submit11/Constants.java
@@ -0,0 +1,33 @@
+package micro.submit11;
+
+import battlecode.common.Direction;
+
+import java.util.Random;
+
+public class Constants {
+    public static final int LAUNCHER_COST_MN = 60;
+    public static final int LAUNCHER_ATTACK_DIS = 16;
+
+    public static final int CARRIER_COST_AD = 50;
+    public static final int CARRIER_ATTACK_DIS = 9;
+
+    /** Array containing all the possible movement directions. */
+    public static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+    /**
+     * A random number generator.
+     * We will use this RNG to make some random moves. The Random class is provided by the java.util.Random
+     * import at the top of this file. Here, we *seed* the RNG with a constant number (6147); this makes sure
+     * we get the same sequence of numbers every time this code is run. This is very useful for debugging!
+     */
+    static final Random rng = new Random(6147);
+}
---
+++ src/micro/submit11/Headquarter.java
@@ -0,0 +1,85 @@
+package micro.submit11;
+
+import battlecode.common.*;
+
+public class Headquarter extends Unit {
+    private static int carrierCnt = 0;
+
+    public static void run() throws GameActionException {
+        if (turnCount == 0) {
+            // first turn all HQ report
+            Comm.HQInit(rc.getLocation(), rc.getID());
+            for (WellInfo well : rc.senseNearbyWells()) {
+                Comm.reportWells(well);
+            }
+            if (Comm.friendlyHQLocations[0].equals(rc.getLocation())) {
+                trySpawn(RobotType.CARRIER, rc.getLocation(), Carrier.SCOUT_SYMMETRY);
+            }
+        }
+
+        if (!Comm.isSymmetryConfirmed && turnCount > 0 && turnCount % 150 == 0) {
+            System.out.println("too long no sym, send another sym scout");
+            trySpawn(RobotType.CARRIER, rc.getLocation(), Carrier.SCOUT_SYMMETRY);
+        }
+
+        if (rc.getRobotCount() >= mapWidth * mapHeight * 0.15) {
+            // this means we have basically won already, build anchor
+            if (rc.canBuildAnchor(Anchor.STANDARD)) {
+                rc.buildAnchor(Anchor.STANDARD);
+            }
+            indicator += "anchoring";
+        } else {
+            // spawn Launchers
+            for (int i = 5; --i >= 0
+                    && rc.isActionReady()
+                    && rc.getResourceAmount(ResourceType.MANA) >= Constants.LAUNCHER_COST_MN;) {
+                Direction dirToCenter = rc.getLocation().directionTo(new MapLocation(mapWidth / 2, mapHeight / 2));
+                MapLocation spawnLocation = rc.getLocation().add(dirToCenter).add(dirToCenter);
+                trySpawn(RobotType.LAUNCHER, spawnLocation, -1);
+            }
+            for (int i = 5; --i >= 0
+                    && rc.isActionReady()
+                    && rc.getResourceAmount(ResourceType.ADAMANTIUM) >= Constants.CARRIER_COST_AD;) {
+                trySpawn(RobotType.CARRIER, rc.getLocation(), ++carrierCnt % 3 == 0? Carrier.MINE_AD : Carrier.MINE_MN);
+            }
+        }
+
+        RobotInfo closestEnemy = null;
+        int dis = Integer.MAX_VALUE;
+        for (RobotInfo robot : rc.senseNearbyRobots(-1, oppTeam)) {
+            if (robot.type == RobotType.LAUNCHER) {
+                int newDis = rc.getLocation().distanceSquaredTo(robot.location);
+                if (newDis < dis) {
+                    closestEnemy = robot;
+                    dis = newDis;
+                }
+            }
+        }
+        if (closestEnemy != null) {
+            Comm.reportEnemy(closestEnemy.location, rc.getRoundNum());
+        }
+        indicator += String.format("enemy %s round %d", Comm.getEnemyLoc(), Comm.getEnemyRound());
+
+        if (turnCount <= 3) { // for symmetry check
+            MapRecorder.recordSym(2000);
+        }
+        Comm.commit_write();
+    }
+
+    // if no need to set spawn flag pass -1
+    private static boolean trySpawn(RobotType robotType, MapLocation center, int spawnFlag) throws GameActionException {
+        for (int[] dir : BFS25) {
+            MapLocation location = new MapLocation(center.x + dir[0], center.y + dir[1]);
+            if(rc.canBuildRobot(robotType, location)
+                    && rc.senseMapInfo(location).getCurrentDirection() == Direction.CENTER) {
+                if (spawnFlag > 0 && !Comm.trySetSpawnFlag(location, spawnFlag)) {
+                    System.out.println("try spawn failed Q full");
+                    return false;
+                }
+                rc.buildRobot(robotType, location);
+                return true;
+            }
+        }
+        return false;
+    }
+}
